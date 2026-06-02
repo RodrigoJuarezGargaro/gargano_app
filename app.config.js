1adc92afca244e8dce5838abcf6e284ba24ddd03
RodrigoJuarezGargaro <rjuarez@gargano.com.ar>
2026-06-02 10:10:10 -0300

    Fix versionado

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: "Gargano Logistica",
     slug: "gargano-logistica",
-    version: "1.0.3",
+    version: "1.0.4",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "garganomobile",
